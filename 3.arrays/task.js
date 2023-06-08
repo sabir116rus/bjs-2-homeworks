fc86dfc14d83e4f3981d7bea989e6209a449008b
Альберт <sabir116rus@gmail.com>
2023-06-08 10:57:03 +0300

    Completed 3 task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(user => user.gender === gender);
+    const totalAge = filteredUsers.map(user => user.age).reduce((sum, age) => sum + age, 0);
+    return filteredUsers.length > 0 ? totalAge / filteredUsers.length : 0;
 }
